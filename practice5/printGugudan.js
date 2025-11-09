6cd900f4037143ac47f630c0e6b72e1905f26270
jun2k5 <jun2k5@naver.com>
2025-11-10 02:12:48 +0900

    251109 오즈코딩 JS 과제 실습

---
+++ practice5/printGugudan.js
@@ -0,0 +1,15 @@
+
+// 힌트 : 변수 left를 선언하고 다른 값을 할당할 때마다
+// 그에 맞는 구구단이 출력되도록 코드를 작성하세요.
+
+let left = 2;
+
+console.log(left * 1);
+console.log(left * 2);
+console.log(left * 3);
+console.log(left * 4);
+console.log(left * 5);
+console.log(left * 6);
+console.log(left * 7);
+console.log(left * 8);
+console.log(left * 9);
